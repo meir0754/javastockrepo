99b8a75d1b02c8f455b03f80aec4ca1334e417a6
meir0754 <meir0754@gmail.com>
2015-03-31 23:54:32 +0300

    Finished exe02

--- src/com/sawdayee/JavastockrepoServlet.java
+++ src/com/sawdayee/JavastockrepoServlet.java
@@ -7,7 +7,11 @@ import javax.servlet.http.*;
 public class JavastockrepoServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		int num1 = 4, num2 = 3, num3 = 7;
+		int result = (num1 + num2) * num3;
+		String resultStr = new String("<h1>Result of ( "+num1+" + "+num2+" ) * "+num3+" = "+result+"</h1>");
+		resp.getWriter().println(resultStr);
+		
 	}
 }
